5b119986933348158454b849f30ba49c470f9c5c
lauritsriple <laurits.triple@gmail.com>
2015-08-28 12:07:50 +0200

    added ledblink in main

--- code_byggern/code_byggern/code_byggern.c
+++ code_byggern/code_byggern/code_byggern.c
@@ -5,13 +5,15 @@
  *  Author: laurittr
  */ 
 
+#define F_CPU 16000000
 
 #include <avr/io.h>
+#include <avr/delay.h>
 
-int main(void)
-{
-    while(1)
-    {
+int main(void) {
+	DDRB |= (1 << PB0);
+    while(1) {
+		PORTB ^= (1 << PB0);
         //TODO:: Please write your application code 
     }
 }
