199d9653daf5709e2229ed1a96c0763fe8c4dd53
lauritsriple <laurits.triple@gmail.com>
2015-08-28 14:16:01 +0200

    Final commit, day1. Everything is working.

--- code_byggern/code_byggern/code_byggern.c
+++ code_byggern/code_byggern/code_byggern.c
@@ -5,12 +5,12 @@
  *  Author: laurittr
  */ 
 
-#define F_CPU 16000000
+#define F_CPU 4915200
 
 #include <avr/io.h>
-#include <avr/delay.h>
+#include <util/delay.h>
 #include "uart.h"
-
+#include <stdio.h>
 
 
 
@@ -20,7 +20,9 @@ int main(void) {
 	DDRB |= (1 << PB0);
     while(1) {
 		PORTB ^= (1 << PB0);
-        uart_putChar(F);
-		_delay_ms(50);
+        uart_putChar('f');
+		puts("ho");
+		printf("test int: %i", 100);
+		_delay_ms(500);
     }
 }
--- code_byggern/code_byggern/uart.c
+++ code_byggern/code_byggern/uart.c
@@ -5,37 +5,43 @@
  *  Author: laurittr
  */ 
 
-//#include <avr/io.h>
+#include <avr/io.h>
 #include "uart.h"
+#include <stdio.h>
 
 void uart_init(void){
-	UBRR0L = 15;	// baudrate 192000hz
-	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+	//UBRR0L = 15;	// baudrate 192000hz
+	UBRR0L = 0b00001111;
 	
 	//Asynchronous operation
-	UCSR0C &= ~(1 << UMSEL0)
+	//UCSR0C &= ~(1 << UMSEL0);
 	
 	//Parity mode
-	UCSR0C &= ~((1 << UPM01)|(1 << UPM00))
+	//UCSR0C &= ~((1 << UPM01)|(1 << UPM00));
 	
 	//Stopbit
-	UCSR0C &= ~(1 << USBS0);
+	//UCSR0C &= ~(1 << USBS0);
 	
 	//Character size
-	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);
-	UCSR0C &= ~(1 << UCSZ02);
+	//UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);
+	//UCSR0B &= ~(1 << UCSZ02);
+	
+	UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+	
+	//for puts and printf
+	fdevopen(uart_putChar, uart_getChar);
 }
 
 
 void uart_putChar(unsigned char c){
-	while(!(UCSR0A) & (1<<UDRE0)); //Wait for empty transmit buffer
-	UDRE0 = c; // Put data in to buffer
+	while (!(UCSR0A & (1<<UDRE0))); //Wait for empty transmit buffer
+	UDR0 = c; // Put data in to buffer
 }
 
 
 unsigned char uart_getChar(){
 	
 	while((UCSR0A) & (1<<UDRE0)); //Wait for "full" transmit buffer
-	return UDRE0;
+	return UDR0;
 }
 
