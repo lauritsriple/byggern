73ecbf3977616bf5ed00e2852f3c6af4098327ad
lauritsriple <laurits.triple@gmail.com>
2015-08-28 12:05:20 +0200

    added atmel projectfiles

---
+++ code_byggern/code_byggern/code_byggern.c
@@ -0,0 +1,17 @@
+/*
+ * code_byggern.c
+ *
+ * Created: 28.08.2015 12:04:28
+ *  Author: laurittr
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
