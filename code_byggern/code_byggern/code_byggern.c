7af82e202ba6954ce2ddc53be873212728263d0a
unknown <shahrukk@itk-d670.win.ntnu.no>
2015-09-04 15:51:22 +0200

    Day 2 Complete - everything is working

--- code_byggern/code_byggern/code_byggern.c
+++ code_byggern/code_byggern/code_byggern.c
@@ -10,19 +10,26 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart.h"
+#include "SRAM_test.h"
 #include <stdio.h>
 
 
 
+
 int main(void) {
-	uart_init();
 	
+	//Initialize uart and debugLED
+	uart_init();									
 	DDRB |= (1 << PB0);
+	MCUCR |= (1 << SRE);
+	
+	SRAM_test();
     while(1) {
-		PORTB ^= (1 << PB0);
-        uart_putChar('f');
-		puts("ho");
-		printf("test int: %i", 100);
+		PORTB ^= (1 << PB0);										//Toggle DebugLED
+        
+		//uart_putChar('f');
+		//puts("ho");
+		//printf("test int: %i", count);
 		_delay_ms(500);
     }
 }
