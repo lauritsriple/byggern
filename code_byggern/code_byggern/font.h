6b1c25d49a2bd7b288d4259baa0679c5341d8a1d
lauritsriple <laurits.triple@gmail.com>
2015-09-18 15:42:16 +0200

    files uploaded

---
+++ code_byggern/code_byggern/font.h
@@ -0,0 +1,17 @@
+/*
+ * font.h
+ *
+ * Created: 18.09.2015 14:42:12
+ *  Author: laurittr
+ */ 
+
+
+#ifndef FONT_H_
+#define FONT_H_
+
+const unsigned char PROGMEM font[95][5];
+
+#define FONT_WIDTH 5
+#define FONT_OFFSET 32
+
+#endif /* FONT_H_ */
---
+++ code_byggern/code_byggern/oled.h
@@ -0,0 +1,25 @@
+/*
+ * oled.h
+ *
+ * Created: 18.09.2015 11:31:21
+ *  Author: laurittr
+ */ 
+
+
+#ifndef OLED_H_
+#define OLED_H_
+
+
+void oled_init(void);
+void oled_home(void);
+void oled_clear_page(uint8_t line);
+void oled_clear_all(void);
+void oled_pos(uint8_t page, uint8_t column);
+void oled_put_char(char c);
+void oled_printf(char* fmt, ...);
+void oled_fill_page(uint8_t page);
+
+void oled_clear_all();
+
+
+#endif /* OLED_H_ */
